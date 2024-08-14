b7c40e533d19544dd5fe77c33c4d6766ccee3083
Dovydas Montvydas <dovism@gmail.com>
2024-08-14 10:55:38 +0300

    repository

---
+++ GrupineUzduotisPostai/Repositories/PostDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupineUzduotisPostai.Core.Repositories
+{
+    public class PostDbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("")
+        }
+    }
+}
