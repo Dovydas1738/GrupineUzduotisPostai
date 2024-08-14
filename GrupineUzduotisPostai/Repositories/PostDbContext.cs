f49391f8dd33a159cc1d61d8c8c8fc682ac80201
Dovydas Montvydas <dovism@gmail.com>
2024-08-14 11:02:15 +0300

    su nuget

--- GrupineUzduotisPostai/Repositories/PostDbContext.cs
+++ GrupineUzduotisPostai/Repositories/PostDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrupineUzduotisPostai.Core.Models;
 
 namespace GrupineUzduotisPostai.Core.Repositories
 {
